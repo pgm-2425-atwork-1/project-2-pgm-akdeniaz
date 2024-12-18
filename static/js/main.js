bf12d63d79cd976732c753f1dbd3844ebb58fbb6
pgm-akdeniaz <akdenis.niaziev@student.arteveldehs.be>
2024-12-18 19:03:39 +0100

    create localStorage for theme-mode memory

--- static/js/main.js
+++ static/js/main.js
@@ -1,30 +1,50 @@
-// Switch between dark/light mode
+// Toggle dark/light mode on click
 document.querySelector(".theme-icon").addEventListener("click", () => {
   const body = document.body;
   const themeIcon = document.querySelector(".theme-icon");
+  const links = document.querySelectorAll("a");
 
   body.classList.toggle("dark-mode");
 
   if (body.classList.contains("dark-mode")) {
+    // Apply dark mode settings
     themeIcon.src = "static/images/icons/sun.svg";
     themeIcon.className = "theme-icon moon-mode";
+    links.forEach((link) => (link.style.color = ""));
+    localStorage.setItem("theme", "dark-mode");
   } else {
+    // Apply light mode settings
     themeIcon.src = "static/images/icons/moon.svg";
     themeIcon.className = "theme-icon sun-mode";
+    links.forEach((link) => (link.style.color = "black"));
+    localStorage.setItem("theme", "light-mode");
   }
 });
 
-// By default, make it dark themed
+// Load saved theme from localStorage
 document.addEventListener("DOMContentLoaded", () => {
   const body = document.body;
   const themeIcon = document.querySelector(".theme-icon");
+  const savedTheme = localStorage.getItem("theme");
+  const links = document.querySelectorAll("a");
 
-  body.classList.add("dark-mode");
-  themeIcon.src = "static/images/icons/sun.svg";
-  themeIcon.className = "theme-icon sun-mode";
+  if (savedTheme === "dark-mode") {
+    // Load dark mode
+    body.classList.add("dark-mode");
+    themeIcon.src = "static/images/icons/sun.svg";
+    themeIcon.className = "theme-icon moon-mode";
+    links.forEach((link) => (link.style.color = ""));
+  } else {
+    // Load light mode
+    body.classList.remove("dark-mode");
+    themeIcon.src = "static/images/icons/moon.svg";
+    themeIcon.className = "theme-icon sun-mode";
+    links.forEach((link) => (link.style.color = "black"));
+  }
 });
 
-document.getElementById("hamburger").addEventListener("click", (event) => {
+// Toggle mobile nav visibility
+document.getElementById("hamburger").addEventListener("click", () => {
   const navHidden = document.getElementById("nav-hidden");
   navHidden.classList.toggle("visible");
 });
